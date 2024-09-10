0a29af83d9b4cce37940f9748498ae956ea566f4
mariosknl <mariosknl@outlook.com>
2024-09-10 12:29:02 +0300

    fetch all users attending an event

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,36 +1,55 @@
 import { Feather } from '@expo/vector-icons';
 import dayjs from 'dayjs';
 import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { View, Text, Image, Pressable } from 'react-native';
+import { supabase } from '~/utils/supabase';
 
-const EventListItem = ({ event }) => (
-  <Link href={`/${event.id}`} asChild>
-    <Pressable className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
-      <View className="flex-row">
-        <View className="flex-1 gap-2">
-          <Text className="text-lg font-semibold uppercase text-amber-800">
-            {dayjs(event.date).format('ddd, D MMM')} · {dayjs(event.date).format('h:mm A')}
-          </Text>
-          <Text className="text-xl font-bold" numberOfLines={2}>
-            {event.title}
-          </Text>
+const EventListItem = ({ event }) => {
+  const [numberOfAttendees, setNumberOfAttendees] = useState(0);
 
-          <Text className="text-gray-700">{event.location}</Text>
+  useEffect(() => {
+    fetchNumberOfAttendees();
+  }, [event.id]);
+
+  const fetchNumberOfAttendees = async () => {
+    const { count } = await supabase
+      .from('attendance')
+      .select('*', { count: 'exact', head: true })
+      .eq('event_id', event.id);
+
+    setNumberOfAttendees(count);
+  };
+
+  return (
+    <Link href={`/event/${event.id}`} asChild>
+      <Pressable className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
+        <View className="flex-row">
+          <View className="flex-1 gap-2">
+            <Text className="text-lg font-semibold uppercase text-amber-800">
+              {dayjs(event.date).format('ddd, D MMM')} · {dayjs(event.date).format('h:mm A')}
+            </Text>
+            <Text className="text-xl font-bold" numberOfLines={2}>
+              {event.title}
+            </Text>
+
+            <Text className="text-gray-700">{event.location}</Text>
+          </View>
+
+          {/* Event Image */}
+          <Image source={{ uri: event.image_uri }} className="aspect-video w-2/5 rounded-xl" />
         </View>
 
-        {/* Event Image */}
-        <Image source={{ uri: event.image_uri }} className="aspect-video w-2/5 rounded-xl" />
-      </View>
+        {/* footer */}
+        <View className="flex-row gap-3">
+          <Text className="mr-auto text-gray-700">{numberOfAttendees} going</Text>
 
-      {/* footer */}
-      <View className="flex-row gap-3">
-        <Text className="mr-auto text-gray-700">16 going</Text>
-
-        <Feather name="share" size={20} color="gray" />
-        <Feather name="bookmark" size={20} color="gray" />
-      </View>
-    </Pressable>
-  </Link>
-);
+          <Feather name="share" size={20} color="gray" />
+          <Feather name="bookmark" size={20} color="gray" />
+        </View>
+      </Pressable>
+    </Link>
+  );
+};
 
 export default EventListItem;
