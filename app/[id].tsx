a750e741dcb9c1d042ffe5f00fe5e494c56dcc02
mariosknl <mariosknl@outlook.com>
2024-09-10 09:44:49 +0300

    RSPV footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Pressable } from 'react-native';
 
 import events from '~/assets/events.json';
 
@@ -14,23 +14,29 @@ export default function EventPage() {
   }
 
   return (
-    <>
+    <View className="flex-1 gap-3 bg-white p-3">
       <Stack.Screen
         options={{ title: 'Event', headerBackTitleVisible: false, headerTintColor: 'black' }}
       />
-      <View className="flex-1 gap-3 bg-white p-3">
-        <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
+      <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
 
-        <Text className="text-3xl font-bold" numberOfLines={2}>
-          {event.title}
-        </Text>
-        <Text className="text-lg font-semibold uppercase text-amber-800">
-          {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
-        </Text>
-        <Text className="text-lg" numberOfLines={2}>
-          {event.description}
-        </Text>
+      <Text className="text-3xl font-bold" numberOfLines={2}>
+        {event.title}
+      </Text>
+      <Text className="text-lg font-semibold uppercase text-amber-800">
+        {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
+      </Text>
+      <Text className="text-lg" numberOfLines={2}>
+        {event.description}
+      </Text>
+
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
       </View>
-    </>
+    </View>
   );
 }
